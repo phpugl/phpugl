5067eafa42673faea36292f4399d3fe4be77e01d
Danilo Kuehn <dk@nogo-software.de>
2012-07-17 10:33:42 +0200

    add composer to page and update readme

--- index.php
+++ index.php
@@ -4,5 +4,5 @@ if (PHP_VERSION_ID < 50300)
 {
     trigger_error("PieCrust requires PHP 5.3+", E_USER_ERROR);
 }
-require '../piecrust.php';
+require 'vendor/bolt80/piecrust/piecrust.php';
 piecrust_run();
