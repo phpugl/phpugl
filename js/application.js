d8be42eb6034905084c64a54ee36d8ed13a70b34
Danilo Kuehn <dk@nogo-software.de>
2014-09-10 17:11:37 +0200

    add next meeting and add english translation

---
+++ js/application.js
@@ -0,0 +1,21 @@
+'use strict';
+
+(function ($) {
+
+  $('.language-switch a').click(function (e) {
+    e.preventDefault();
+
+    var $el = $(this),
+        parentSwitch = $el.parents('.language-switch'),
+        parentArticle = $el.parents('article'),
+        activeBtn = $('a.btn-success', parentSwitch);
+
+
+    $('*[lang="' + activeBtn.data('lang') + '"]', parentArticle).addClass('hide');
+    activeBtn.removeClass('btn-success');
+    $('*[lang="' + $el.data('lang') + '"]', parentArticle).removeClass('hide');
+    $el.addClass('btn-success');
+  });
+
+  $('*[lang="en"]').addClass('hide');
+}) (jQuery);
